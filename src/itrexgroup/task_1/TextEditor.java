8a41108d62870c2ebe044d0caf31ccfb36119b37
Siarhei Stasiulevich <deform@tut.by>
2021-08-18 20:41:35 +0300

    Method "removeCFromTheText" have been added
    Method "removeCFromTheText" removes the "c" character from the text.

--- src/itrexgroup/task_1/TextEditor.java
+++ src/itrexgroup/task_1/TextEditor.java
@@ -1,4 +1,23 @@
 package itrexgroup.task_1;
 
 public class TextEditor {
+
+    private final static String CE = "ce";
+    private final static String CI = "ci";
+    private final static String CK = "ck";
+    private final static String C = "c";
+    private final static String SE = "se";
+    private final static String SI = "si";
+    private final static String K = "k";
+
+
+    private String removeCFromTheText(String text){
+
+        String timeLine_1 = text.replaceAll(CE, SE);
+        String timeLine_2 = timeLine_1.replaceAll(CI, SI);
+        String timeLine_3 = timeLine_2.replaceAll(CK, K);
+
+        return timeLine_3.replaceAll(C, K);
+
+    }
 }
