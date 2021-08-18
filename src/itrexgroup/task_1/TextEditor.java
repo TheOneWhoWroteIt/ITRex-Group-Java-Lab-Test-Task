4e365652cf9d26d2dd1425c8e853fd86bd1d16ee
Siarhei Stasiulevich <deform@tut.by>
2021-08-18 20:56:56 +0300

    Method "convertsDoubleLettersIntoOne" have been added

--- src/itrexgroup/task_1/TextEditor.java
+++ src/itrexgroup/task_1/TextEditor.java
@@ -1,5 +1,8 @@
 package itrexgroup.task_1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TextEditor {
 
     private final static String CE = "ce";
@@ -9,6 +12,11 @@ public class TextEditor {
     private final static String SE = "se";
     private final static String SI = "si";
     private final static String K = "k";
+    private final static String OO = "oo";
+    private final static String EE = "ee";
+    private final static String U = "u";
+    private final static String I = "i";
+    private final static String REGEX = "(\\D)\\1+";
 
 
     private String removeCFromTheText(String text){
@@ -19,5 +27,27 @@ public class TextEditor {
 
         return timeLine_3.replaceAll(C, K);
 
+    }
+
+    private String convertsDoubleLettersIntoOne(String text){
+
+        StringBuilder result = new StringBuilder();
+
+        String[] array = text.split(" ");
+
+        for (String s : array) {
+            Pattern p = Pattern.compile(REGEX);
+            Matcher m = p.matcher(s);
+
+            if (m.find()) {
+
+                result.append(s.replaceAll(REGEX, "$1")).append(" ");
+
+            } else {
+                result.append(s).append(" ");
+            }
+        }
+
+        return result.toString();
     }
 }
