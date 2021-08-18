c2eef3aa35ac6133c402b9c908706ab1270940fd
Siarhei Stasiulevich <deform@tut.by>
2021-08-18 22:16:56 +0300

    Class "RunnerTaskForTest" have been added
    The class is needed to check the program's operation.

---
+++ src/itrexgroup/task_1/RunnerTaskForTest.java
@@ -0,0 +1,11 @@
+package itrexgroup.task_1;
+
+public class RunnerTaskForTest {
+
+    public static void main(String[] args) {
+
+        TextEditor textEditor = new TextEditor();
+        String result = textEditor.editText("success cacao and coffeei aandd 123444556777 e the apple a table an orange");
+        System.out.println(result);
+    }
+}
