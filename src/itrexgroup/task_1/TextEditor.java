19e1312b740f3824611e8f6f89902dc6cc7b4889
Siarhei Stasiulevich <deform@tut.by>
2021-08-19 20:42:26 +0300

    Refactor
    Methods "removeCFromTheText", "removeDoubleLetter", "editText" have been simplified.

--- src/itrexgroup/task_1/TextEditor.java
+++ src/itrexgroup/task_1/TextEditor.java
@@ -5,31 +5,27 @@ import java.util.regex.Pattern;
 
 public class TextEditor {
 
-    private final static String CE = "ce";
-    private final static String CI = "ci";
-    private final static String CK = "ck";
-    private final static String C = "c";
-    private final static String SE = "se";
-    private final static String SI = "si";
-    private final static String K = "k";
-    private final static String OO = "oo";
-    private final static String EE = "ee";
-    private final static String U = "u";
-    private final static String I = "i";
-    private final static String REGEX = "(\\D)\\1+";
-    private final static String E = "e";
-    private final static String A = "a";
-    private final static String AN = "an";
-    private final static String TH = "th";
+    private final String CE = "ce";
+    private final String CI = "ci";
+    private final String CK = "ck";
+    private final String C = "c";
+    private final String SE = "se";
+    private final String SI = "si";
+    private final String K = "k";
+    private final String OO = "oo";
+    private final String EE = "ee";
+    private final String U = "u";
+    private final String I = "i";
+    private final String REGEX = "(\\D)\\1+";
+    private final String E = "e";
+    private final String A = "a";
+    private final String AN = "an";
+    private final String TH = "th";
 
 
     private String removeCFromTheText(String text){
 
-        String timeLine_1 = text.replaceAll(CE, SE);
-        String timeLine_2 = timeLine_1.replaceAll(CI, SI);
-        String timeLine_3 = timeLine_2.replaceAll(CK, K);
-
-        return timeLine_3.replaceAll(C, K);
+        return text.replaceAll(CE, SE).replaceAll(CI, SI).replaceAll(CK, K).replaceAll(C, K);
 
     }
 
@@ -56,10 +52,8 @@ public class TextEditor {
     }
 
     private String removeDoubleLetter(String text){
-        String timeLine_1 = text.replaceAll(OO, U);
-        String timeLine_2 = timeLine_1.replaceAll(EE, I);
 
-        return convertsDoubleLettersIntoOne(timeLine_2);
+        return convertsDoubleLettersIntoOne(text.replaceAll(OO, U).replaceAll(EE, I));
     }
 
     private String removeTheLetterEAtTheEndOfWord(String text){
@@ -91,9 +85,7 @@ public class TextEditor {
     }
 
     public String editText(String text){
-        String timeLine_1 = removeCFromTheText(text);
-        String timeLine_2 = removeDoubleLetter(timeLine_1);
-        String timeLine_3 = removeTheLetterEAtTheEndOfWord(timeLine_2);
-        return removeArticles(timeLine_3);
+
+        return removeArticles(removeTheLetterEAtTheEndOfWord(removeDoubleLetter(removeCFromTheText(text))));
     }
 }
