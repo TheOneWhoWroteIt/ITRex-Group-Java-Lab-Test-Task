b518180bb85c72e24040da03616fa97e32f87f5d
Siarhei Stasiulevich <deform@tut.by>
2021-08-21 21:12:45 +0300

    Class "RunnerTaskForTest" have been added

---
+++ src/itrexgroup/task_2/RunnerTaskForTest.java
@@ -0,0 +1,10 @@
+package itrexgroup.task_2;
+
+public class RunnerTaskForTest {
+
+    public static void main(String[] args) {
+
+        GameLogic gameLogic = new GameLogic();
+        System.out.println(gameLogic.getTime());
+    }
+}
